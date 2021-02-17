995c32a13fa1f206c03da17e90e7afa97091a9c7
jlcalderon <jlcalderonfuentes@gmail.com>
2021-02-17 16:39:38 -0600

    Deletion of registration page because is not necesary

--- auto-loan/src/App.js
+++ auto-loan/src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
-import Registration from "./components/pages/Registration";
-import Disqualification from "./components/pages/Disqualification";
 import ApplicationReults from "./components/pages/ApplicationResults";
+import Disqualification from "./components/pages/Disqualification";
 
 function App() {
   return (
     <div>
       <Router>
         <Route exact path="/" component={Home} />
-        <Route exact path="/registration" component={Registration} />
         <Route exact path="/myapplication" component={ApplicationReults} />
         <Route exact path="/disqualification" component={Disqualification} />
       </Router>
