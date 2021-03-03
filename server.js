85ba268ce1cee69486c362e6d63bd5640397a186
jlcalderon <jlcalderonfuentes@gmail.com>
2021-03-03 06:59:18 -0600

    Correcting typo of double // on server.js line 12

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static("client/build"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client//build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
