1357f83911d18cc1cafea156c75519a0453ebd70
jlcalderon <jlcalderonfuentes@gmail.com>
2021-03-03 06:57:18 -0600

    Removing if wrapper for production logic making the default static files of express the build folder inside the client

--- server.js
+++ server.js
@@ -6,12 +6,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client"));
-}
+app.use(express.static("client/build"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/index.html"));
+  res.sendFile(path.join(__dirname, "./client//build/index.html"));
 });
 
 app.listen(PORT, () => {
