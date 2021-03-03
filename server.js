7a24ef5228b391310c99e8bcd0a8ea0151f64f80
jlcalderon <jlcalderonfuentes@gmail.com>
2021-03-03 06:50:54 -0600

    Fixing issue with accessing static pages on client front-end side

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client"));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/index.html"));
 });
 
 app.listen(PORT, () => {
