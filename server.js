f59a727f29a9803a3882ad1ee464d506effb100e
jlcalderon <jlcalderonfuentes@gmail.com>
2021-03-03 07:19:14 -0600

    Changing url of the express static path on the server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("client/build"));
+app.use(express.static("client"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/index.html"));
 });
 
 app.listen(PORT, () => {
