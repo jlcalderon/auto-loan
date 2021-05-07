185fcf1c01f308961555a3bfc0a646cf4e58fecc
jlcalderon <jlcalderonfuentes@gmail.com>
2021-05-06 21:52:41 -0500

    Adding unit test of app rendering with initial state

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Renders the App landing page with initial state", () => {
+  //Mock all initial state
+  const initialState = {
+    autoLoanApplication: {
+      loanApplication: {},
+      uiHints:
+        "Hi there! I will be here to help you fill out your loan pre qualification form", //Help users by displaying message when they are wrong or rigth typing the info in the form
+      updatedPurchasePrice: 0,
+      autoMake: "",
+      autoModel: "",
+      userEstimatedIncome: 0,
+      userCreditScore: 600,
+      flags: 0,
+    },
+    autoLoanApplicationResults: {
+      applicationResults: {},
+    },
+  };
+  const mockStore = configureStore();
+
+  //Render the app wrapped in the context provider with the store of the mocked state
+  render(
+    <Provider store={mockStore(initialState)}>
+      <App />
+    </Provider>
+  );
+  //Assert wheater the labelElement text was found in the document
+  const uiHintsText = screen.getByText(
+    initialState.autoLoanApplication.uiHints
+  );
+  expect(uiHintsText).toBeInTheDocument();
 });
